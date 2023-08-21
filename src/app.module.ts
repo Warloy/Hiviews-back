84ed189dde5415819081415974a10826ed1a8201
rinzldev <oswaldo.contacto@protonmail.com>
2023-08-20 21:52:48 -0400

    add: some CRUDs + more settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { UserModule } from './user/user.module';
+import { ReviewModule } from './review/review.module';
+ 
+//paquetes de node usualmente van al inicio.
 
+@Module({ 
+  imports: [ 
+    ServeStaticModule.forRoot({ 
+         rootPath: join(__dirname,'..','public'), 
+    }), UserModule, ReviewModule, 
+    // En esta parte se va configurar la conexion con la BD
+     
+  ], 
 
-@Module({
-  imports: [],
   
-  
-})
-export class AppModule {}
+
+}) 
+export class AppModule {} 
