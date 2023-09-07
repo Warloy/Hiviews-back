1a86bbe4f68d4ac786c194350658eae06e7d600c
rinzldev <oswaldo.contacto@protonmail.com>
2023-09-07 10:11:08 -0400

    add: docker-compose + Mongo BD connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { join } from 'path';
+// Mantener los paquetes de nest en la parte superior
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+
+
 import { UserModule } from './user/user.module';
 import { ReviewModule } from './review/review.module';
  
@@ -12,6 +16,7 @@ import { ReviewModule } from './review/review.module';
          rootPath: join(__dirname,'..','public'), 
     }), UserModule, ReviewModule, 
     // En esta parte se va configurar la conexion con la BD
+    MongooseModule.forRoot('mongodb://localhost:27017/backend-hiviews')
      
   ], 
 
