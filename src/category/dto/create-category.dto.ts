2d59faaf74db0b2b411fa0fb568ecab85cc9672e
rinzldev <oswaldo.contacto@protonmail.com>
2023-10-02 14:47:18 -0400

    updated: reviewcolletion

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,1 +1,11 @@
-export class CreateCategoryDto {}
+import { IsIn, IsString } from "class-validator";
+
+export class CreateCategoryDto {
+
+    @IsString()
+    @IsIn(['Cinéfilo','Discusión', 'Memes','Offtopic','Trending',])
+    name: string;
+
+    
+
+}
--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,1 +1,21 @@
-export class Category {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+import { Thread } from 'src/thread/entities/thread.entity';
+
+@Schema()
+export class Category extends Document {
+  @Prop({
+    unique: true,
+    required: true,
+    index: true,
+  })
+  name: string;
+
+  // Add any other properties relevant to your Category entity
+
+  @Prop({type: [{type: Types.ObjectId, ref: 'Thread.name' , required: true}] })
+    reviews: Types.Array<Thread>;
+
+}
+
+export const CategorySchema = SchemaFactory.createForClass(Category);
