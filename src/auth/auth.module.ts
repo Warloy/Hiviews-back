b8d49960d689672791e6db6995dafdb80c07e2e9
rinzldev <oswaldo.contacto@protonmail.com>
2023-10-02 13:50:07 -0400

    added: extendent time

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -26,7 +26,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
           return {
             secret: configService.get('JWT_SECRET') ,
             signOptions: {
-              expiresIn: "1h"
+              expiresIn: "2d" 
             }
           }
         }
